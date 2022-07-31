b936b14ec44f52c08d3b88b716ded769f0eec064
quintana29 <jhonatanquintana29@gmail.com>
2022-07-30 22:28:48 -0500

    Se comento el ultimo controlador creado

--- backend/src/main/java/org/sofka/mykrello/controller/LogController.java
+++ backend/src/main/java/org/sofka/mykrello/controller/LogController.java
@@ -6,17 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+/**
+ * Controlador con los endpoints del Log
+ */
 @CrossOrigin(value = "*")
 @RestController
 @RequestMapping("/api/v1/log")
 public class LogController {
-
+    /**
+     * Inyeccion de dependencia de MyResponseUtility
+     */
     @Autowired
     private MyResponseUtility response;
+    /**
+     * Inyeccion de dependencia de LogService
+     */
     @Autowired
     private LogService logService;
-
+    /**
+     * Metodo para obtener todos los Logs por tareas
+     * @return Una lista de objetos de tipo ResponseEntity con el Board y el codigo HttpStatus
+     */
     @GetMapping(path = "/{idTask}")
     public ResponseEntity<MyResponseUtility> index(@PathVariable("idTask") Integer idTask) {
         response.data = logService.findById(idTask);
